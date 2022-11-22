ca2d529df50845e7df11e0e98af729633470be46
Maxence Maireaux <maxence@maireaux.fr>
2022-11-22 12:08:34 +0100

    feat: Add more env vars for Webhooks Worker

--- controllers/components/webhooks/webhooks_mutator.go
+++ controllers/components/webhooks/webhooks_mutator.go
@@ -202,6 +202,7 @@ func (r *Mutator) reconcileWorkersDeployment(ctx context.Context, webhooks *comp
 		Env("KAFKA_SASL_MECHANISM", webhooks.Spec.Collector.SASL.Mechanism),
 		Env("KAFKA_USERNAME", webhooks.Spec.Collector.SASL.Username),
 		Env("KAFKA_PASSWORD", webhooks.Spec.Collector.SASL.Password),
+		Env("KAFKA_CONSUMER_GROUP", webhooks.GetName()),
 	}
 
 	if webhooks.Spec.Debug {
