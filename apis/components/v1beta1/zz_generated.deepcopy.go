99f1d76d8e0d4d353410440d62541d86d82d2a8d
Maxence Maireaux <maxence@maireaux.fr>
2022-11-28 21:26:33 +0100

    chore: Print versions use in Stack object

--- apis/components/v1beta1/zz_generated.deepcopy.go
+++ apis/components/v1beta1/zz_generated.deepcopy.go
@@ -23,7 +23,6 @@ package v1beta1
 
 import (
 	"encoding/json"
-
 	auth_componentsv1beta1 "github.com/numary/operator/apis/auth.components/v1beta1"
 	apisv1beta1 "github.com/numary/operator/pkg/apis/v1beta1"
 	runtime "k8s.io/apimachinery/pkg/runtime"
