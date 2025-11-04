fb37dc3f91602f8150d01f2134cfb71d6aaac654
Maxence Maireaux <maxence@maireaux.fr>
2025-11-04 10:19:56 +0100

    test: add comprehensive tests for NODE_IP injection
    Add unit and integration tests to verify that NODE_IP environment variable
    is correctly injected in all containers and init containers using the
    Kubernetes downward API (status.hostIP).
    
    Test coverage includes:
    - Empty deployments
    - Deployments with existing environment variables
    - Deployments with init containers
    - Deployments with multiple containers
    
    Also includes formatting fixes from make fmt.

--- internal/resources/applications/application_test.go
+++ internal/resources/applications/application_test.go
@@ -62,3 +62,143 @@ func TestWithAnnotations(t *testing.T) {
 	}
 
 }
+
+func TestWithNodeIP(t *testing.T) {
+	t.Parallel()
+	type testCase struct {
+		name       string
+		deployment *appsv1.Deployment
+	}
+
+	testCases := []testCase{
+		{
+			name: "empty deployment",
+			deployment: &appsv1.Deployment{
+				Spec: appsv1.DeploymentSpec{
+					Template: v1.PodTemplateSpec{
+						Spec: v1.PodSpec{
+							Containers: []v1.Container{
+								{
+									Name:  "main",
+									Image: "test:latest",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "deployment with existing env vars",
+			deployment: &appsv1.Deployment{
+				Spec: appsv1.DeploymentSpec{
+					Template: v1.PodTemplateSpec{
+						Spec: v1.PodSpec{
+							Containers: []v1.Container{
+								{
+									Name:  "main",
+									Image: "test:latest",
+									Env: []v1.EnvVar{
+										{Name: "EXISTING_VAR", Value: "existing_value"},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "deployment with init containers",
+			deployment: &appsv1.Deployment{
+				Spec: appsv1.DeploymentSpec{
+					Template: v1.PodTemplateSpec{
+						Spec: v1.PodSpec{
+							InitContainers: []v1.Container{
+								{
+									Name:  "init",
+									Image: "init:latest",
+								},
+							},
+							Containers: []v1.Container{
+								{
+									Name:  "main",
+									Image: "test:latest",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "deployment with multiple containers",
+			deployment: &appsv1.Deployment{
+				Spec: appsv1.DeploymentSpec{
+					Template: v1.PodTemplateSpec{
+						Spec: v1.PodSpec{
+							InitContainers: []v1.Container{
+								{
+									Name:  "init1",
+									Image: "init1:latest",
+								},
+								{
+									Name:  "init2",
+									Image: "init2:latest",
+								},
+							},
+							Containers: []v1.Container{
+								{
+									Name:  "main",
+									Image: "test:latest",
+								},
+								{
+									Name:  "sidecar",
+									Image: "sidecar:latest",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			app := Application{}
+			require.NoError(t, app.withNodeIP(nil)(tc.deployment))
+
+			// Verify NODE_IP is injected in all init containers
+			for i, container := range tc.deployment.Spec.Template.Spec.InitContainers {
+				found := false
+				for _, env := range container.Env {
+					if env.Name == "NODE_IP" {
+						found = true
+						require.NotNil(t, env.ValueFrom, "NODE_IP should use ValueFrom for init container %d", i)
+						require.NotNil(t, env.ValueFrom.FieldRef, "NODE_IP should use FieldRef for init container %d", i)
+						require.Equal(t, "status.hostIP", env.ValueFrom.FieldRef.FieldPath, "NODE_IP should reference status.hostIP for init container %d", i)
+						break
+					}
+				}
+				require.True(t, found, "NODE_IP env var not found in init container %d", i)
+			}
+
+			// Verify NODE_IP is injected in all containers
+			for i, container := range tc.deployment.Spec.Template.Spec.Containers {
+				found := false
+				for _, env := range container.Env {
+					if env.Name == "NODE_IP" {
+						found = true
+						require.NotNil(t, env.ValueFrom, "NODE_IP should use ValueFrom for container %d", i)
+						require.NotNil(t, env.ValueFrom.FieldRef, "NODE_IP should use FieldRef for container %d", i)
+						require.Equal(t, "status.hostIP", env.ValueFrom.FieldRef.FieldPath, "NODE_IP should reference status.hostIP for container %d", i)
+						break
+					}
+				}
+				require.True(t, found, "NODE_IP env var not found in container %d", i)
+			}
+		})
+	}
+}
--- internal/tests/application_test.go
+++ internal/tests/application_test.go
@@ -58,4 +58,41 @@ var _ = Context("When creating a Application", func() {
 		}).Should(HaveKeyWithValue("annotations", "annotations"))
 	})
 
+	It("Should inject NODE_IP env var in all containers", func() {
+		Eventually(func(g Gomega) {
+			deployment := &appsv1.Deployment{}
+			g.Expect(LoadResource(stack.Name, "ledger", deployment)).To(Succeed())
+
+			// Check init containers
+			for _, container := range deployment.Spec.Template.Spec.InitContainers {
+				found := false
+				for _, env := range container.Env {
+					if env.Name == "NODE_IP" {
+						found = true
+						g.Expect(env.ValueFrom).NotTo(BeNil(), "NODE_IP should use ValueFrom in init container %s", container.Name)
+						g.Expect(env.ValueFrom.FieldRef).NotTo(BeNil(), "NODE_IP should use FieldRef in init container %s", container.Name)
+						g.Expect(env.ValueFrom.FieldRef.FieldPath).To(Equal("status.hostIP"), "NODE_IP should reference status.hostIP in init container %s", container.Name)
+						break
+					}
+				}
+				g.Expect(found).To(BeTrue(), "NODE_IP env var not found in init container %s", container.Name)
+			}
+
+			// Check regular containers
+			for _, container := range deployment.Spec.Template.Spec.Containers {
+				found := false
+				for _, env := range container.Env {
+					if env.Name == "NODE_IP" {
+						found = true
+						g.Expect(env.ValueFrom).NotTo(BeNil(), "NODE_IP should use ValueFrom in container %s", container.Name)
+						g.Expect(env.ValueFrom.FieldRef).NotTo(BeNil(), "NODE_IP should use FieldRef in container %s", container.Name)
+						g.Expect(env.ValueFrom.FieldRef.FieldPath).To(Equal("status.hostIP"), "NODE_IP should reference status.hostIP in container %s", container.Name)
+						break
+					}
+				}
+				g.Expect(found).To(BeTrue(), "NODE_IP env var not found in container %s", container.Name)
+			}
+		}).Should(Succeed())
+	})
+
 })
