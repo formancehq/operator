4cb8083448e8031a937bd7deede28169998c66d4
Sylvain Rabot <sylvain@formance.com>
2025-11-17 14:12:19 +0100

    feat: add setting gateway.caddyfile.trusted-proxies (#361)
    Signed-off-by: Sylvain Rabot <sylvain@formance.com>

--- internal/resources/gateways/caddyfile.go
+++ internal/resources/gateways/caddyfile.go
@@ -1,14 +1,18 @@
 package gateways
 
 import (
+	"strings"
+
 	"github.com/formancehq/go-libs/v2/collectionutils"
 	"github.com/formancehq/operator/api/formance.com/v1beta1"
 	"github.com/formancehq/operator/internal/core"
 	"github.com/formancehq/operator/internal/resources/caddy"
 )
 
+type CaddyOptions func(data map[string]any) error
+
 func CreateCaddyfile(ctx core.Context, stack *v1beta1.Stack,
-	gateway *v1beta1.Gateway, httpAPIs []*v1beta1.GatewayHTTPAPI, broker *v1beta1.Broker) (string, error) {
+	gateway *v1beta1.Gateway, httpAPIs []*v1beta1.GatewayHTTPAPI, broker *v1beta1.Broker, options ...CaddyOptions) (string, error) {
 
 	data := map[string]any{
 		"Services": collectionutils.Map(httpAPIs, func(from *v1beta1.GatewayHTTPAPI) v1beta1.GatewayHTTPAPISpec {
@@ -27,5 +31,25 @@ func CreateCaddyfile(ctx core.Context, stack *v1beta1.Stack,
 		data["Broker"] = broker.Status.URI.Scheme
 	}
 
+	for _, option := range options {
+		if err := option(data); err != nil {
+			return "", err
+		}
+	}
+
 	return caddy.ComputeCaddyfile(ctx, stack, Caddyfile, data)
+}
+
+func withTrustedProxies(options []string) func(data map[string]any) error {
+	return func(data map[string]any) error {
+		data["TrustedProxies"] = strings.Join(options, " ")
+		return nil
+	}
+}
+
+func withTrustedProxiesStrict() func(data map[string]any) error {
+	return func(data map[string]any) error {
+		data["TrustedProxiesStrict"] = true
+		return nil
+	}
 }
--- internal/resources/gateways/configuration.go
+++ internal/resources/gateways/configuration.go
@@ -3,6 +3,7 @@ package gateways
 import (
 	"github.com/formancehq/operator/api/formance.com/v1beta1"
 	"github.com/formancehq/operator/internal/core"
+	"github.com/formancehq/operator/internal/resources/settings"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
 )
@@ -10,7 +11,25 @@ import (
 func createConfigMap(ctx core.Context, stack *v1beta1.Stack,
 	gateway *v1beta1.Gateway, httpAPIs []*v1beta1.GatewayHTTPAPI, broker *v1beta1.Broker) (*v1.ConfigMap, error) {
 
-	caddyfile, err := CreateCaddyfile(ctx, stack, gateway, httpAPIs, broker)
+	options := []CaddyOptions{}
+
+	trustedProxies, err := settings.GetStringSlice(ctx, stack.Name, "gateway", "caddyfile", "trusted-proxies")
+	if err != nil {
+		return nil, err
+	}
+	if trustedProxies != nil {
+		options = append(options, withTrustedProxies(trustedProxies))
+	}
+
+	trustedProxiesStrict, err := settings.GetBool(ctx, stack.Name, "gateway", "caddyfile", "trusted-proxies-strict")
+	if err != nil {
+		return nil, err
+	}
+	if trustedProxiesStrict != nil && *trustedProxiesStrict {
+		options = append(options, withTrustedProxiesStrict())
+	}
+
+	caddyfile, err := CreateCaddyfile(ctx, stack, gateway, httpAPIs, broker, options...)
 	if err != nil {
 		return nil, err
 	}
