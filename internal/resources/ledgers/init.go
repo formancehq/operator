108a57c1f07e14091f55f4ce226b1d6b3903d6be
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2025-12-11 12:55:13 +0100

    chore: remove obsolete code

--- internal/resources/ledgers/init.go
+++ internal/resources/ledgers/init.go
@@ -24,8 +24,6 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/formancehq/operator/api/formance.com/v1beta1"
 	. "github.com/formancehq/operator/internal/core"
