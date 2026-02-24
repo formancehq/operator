13d39622085cedb0a9734d8fc9e1ce0a90e2502c
Sylvain Rabot <sylvain@formance.com>
2026-02-24 15:48:12 +0100

    build: tune build
    Signed-off-by: Sylvain Rabot <sylvain@formance.com>

--- tools/utils/cmd/root.go
+++ tools/utils/cmd/root.go
@@ -9,9 +9,16 @@ import (
 	"github.com/formancehq/go-libs/v2/service"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "utils",
-	Short: "A cli for operator operations",
+var (
+	Version   = "dev"
+	Commit    = "unknown"
+	BuildDate = "unknown"
+)
+
+var RootCmd = &cobra.Command{
+	Use:     "utils",
+	Short:   "A cli for operator operations",
+	Version: Version,
 	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 		logger := logging.NewDefaultLogger(cmd.OutOrStdout(), service.IsDebug(cmd), false, false)
 		logger.Infof("Starting application")
@@ -24,13 +31,9 @@ var rootCmd = &cobra.Command{
 	},
 }
 
-func Execute() {
-	service.Execute(rootCmd)
-}
-
 func init() {
-	rootCmd.AddCommand(NewDatabaseCommand())
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	RootCmd.AddCommand(NewDatabaseCommand())
+	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 
-	service.AddFlags(rootCmd.PersistentFlags())
+	service.AddFlags(RootCmd.PersistentFlags())
 }
--- tools/utils/main.go
+++ tools/utils/main.go
@@ -1,10 +1,15 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-*/
 package main
 
-import "github.com/formancehq/operator/utils/v3/cmd"
+import (
+	"fmt"
+	"os"
+
+	"github.com/formancehq/operator/tools/utils/v3/cmd"
+)
 
 func main() {
-	cmd.Execute()
+	if err := cmd.RootCmd.Execute(); err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
