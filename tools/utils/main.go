0b61909175a97218171ccfc15d8140b36a3f8c56
Paul Nicolas <paul.nicolas.go@gmail.com>
2026-02-25 16:10:29 +0100

    fix(operator-utils): fix missing bind env (#407)

--- tools/utils/main.go
+++ tools/utils/main.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/formancehq/go-libs/v2/service"
+
 	"github.com/formancehq/operator/tools/utils/v3/cmd"
 )
 
 func main() {
+	service.BindEnvToCommand(cmd.RootCmd)
 	if err := cmd.RootCmd.Execute(); err != nil {
 		_, _ = fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
