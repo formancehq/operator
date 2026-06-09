0ea83480c61797379ba784bb1143d115b4873f08
Maxence Maireaux <maxence@formance.com>
2026-06-09 22:14:12 +0200

    fix(kubectl-stacks): return marshal error in disable instead of panicking
    Same rationale as enable: report failures through the command's
    normal error path.

--- tools/kubectl-stacks/disable.go
+++ tools/kubectl-stacks/disable.go
@@ -33,7 +33,7 @@ func disable(cmd *cobra.Command, client *rest.RESTClient, name string) error {
 		},
 	})
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	return client.Patch(types.MergePatchType).
